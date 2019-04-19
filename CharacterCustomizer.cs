41962af6d7ab5f9c98d1e2876d6f6d0a1d7b1df1
AsterAether <tommi99.tn@gmail.com>
2019-04-19 04:19:50 +0200

    Updated versions

--- CharacterCustomizer.cs
+++ CharacterCustomizer.cs
@@ -14,7 +14,7 @@ namespace CharacterCustomizer
 {
     [BepInDependency("com.bepis.r2api")]
     [BepInDependency("at.aster.aetherlib")]
-    [BepInPlugin("at.aster.charactercustomiser", "CharacterCustomizer", "0.0.1")]
+    [BepInPlugin("at.aster.charactercustomiser", "CharacterCustomizer", "0.0.2")]
     public class CharacterCustomizer : BaseUnityPlugin
     {
         private List<CustomSurvivor> CustomSurvivors { get; set; }
